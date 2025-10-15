995121bb6c1155f6277aaf6003a94d78fcc08f0a
Kingsley <cjkings5050@gmail.com>
2025-10-15 09:34:19 +0100

    Updated App.jsx routing and dashboard integration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Owner from "./Component/Owner";
 import Navbar from "./Component/Navbar";
+import AdminDashboard from "./Component/AdminDashboard";
+import Applicants from "./Component/Applicants";
+import PostJob from "./Component/PostJob";
+import MyJobs from "./Component/MyJobs";
 
 
 export default function App() {
   return (
-    <>
-      
-        <Navbar/>
-        <Owner  />
-      
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Owner />} /> {/* Home/Owner page */}
+        <Route path="/admin" element={<AdminDashboard />} /> {/* Admin Dashboard */}
+        <Route path="/applicants" element={<Applicants />} /> {/* Applicants page */}
+        <Route path="/post-job" element={<PostJob />} />
+        <Route path="/myjobs" element={<MyJobs />} />
+
+      </Routes>
+    </Router>
   );
 }
